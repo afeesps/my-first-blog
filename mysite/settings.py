3a2e05bc92c1dad29b03c40d1fb48a77448dcad1
afeesps <afeesps@gmail.com>
2016-04-21 00:48:52 +0530

    Modified blog after home page

--- mysite/settings.py
+++ mysite/settings.py
@@ -53,6 +53,15 @@ MIDDLEWARE_CLASSES = [
 
 ROOT_URLCONF = 'mysite.urls'
 
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'templates').replace('\\','/'),
+)
+
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
