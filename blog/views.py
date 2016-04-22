7741c37a16c8bd09a28231d1e03b4286b5d1d584
afeesps <afeesps@gmail.com>
2016-04-22 23:01:20 +0530

    Bootstrap ThemeCommit

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.template.context_processors import request
+#from .models import Post
+from django.utils import timezone
 
 
 # Create your views here.
 def post_list(request):
-    
-  return render(request,'blog/post_list.html',{})
+     
+  #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+  # print posts
+  # return render(request,'blog/post_list.html',{'posts':posts})
+  return render(request,'blog/index.html',{})
+ 
